0be40ca730ab56333f926651ac1cc5889201519d
Erik Menezes <erikcarlos.menezes@gmail.com>
2025-10-23 22:39:36 -0300

    Delete POO/AV2 directory

--- POO/AV2/Questão2.py
+++
@@ -1,2 +0,0 @@
-numero = input("Digite um número: ")
-print(f"O número informado foi {numero}")
--- POO/AV2/Questão4.py
+++
@@ -1,8 +0,0 @@
-notas = []
-
-for i in range(4):
-    nota = float(input(f"Digite a nota {i + 1}: "))
-    notas.append(nota)
-
-media = sum(notas) / 4
-print("Sua média é:", media)
